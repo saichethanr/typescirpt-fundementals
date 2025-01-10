413461f311327344eb2785b1051724956b7209d3
saichethanr <saichethan9019@gmail.com>
2025-01-10 21:14:21 +0530

    number string boolean

---
+++ datatypes.js
@@ -0,0 +1,18 @@
+"use strict";
+//type programming language
+//string
+let lname;
+lname = "SANTOSH";
+let newname = lname.toLowerCase();
+console.log(newname);
+//number 
+let age = 20;
+let age1 = 10;
+age = 60;
+let dob = "25.90";
+//aprse float returns a float
+let result = parseFloat(dob);
+// age="sai"
+//boolean
+let bool = true; //its not definned when declared to false
+console.log(bool); // the bool is not beeing asigned
---
+++ datatypes.ts
@@ -0,0 +1,20 @@
+//type programming language
+
+//string
+let lname : string;
+lname = "SANTOSH";
+let newname : string = lname.toLowerCase();
+console.log(newname);
+
+//number 
+let age = 20;
+let age1 : number = 10;
+age =60;
+let dob : string = "25.90"
+//aprse float returns a float
+let result = parseFloat(dob);
+// age="sai"
+
+//boolean
+let bool : boolean = true; //its not definned when declared to false -- this is due to strict = true in the .json
+console.log(bool); // the bool is not beeing asigned
